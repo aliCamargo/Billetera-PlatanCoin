5287bd16a9a2e489fd3a351529b9bbf76b991418
Ali Camargo <ali.camargo@hotmail.com>
2016-10-17 14:47:13 -0500

    install gems for testing

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,21 @@ module BilleteraPlatancoin
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    #-- Rspec gem
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       helper: false,
+                       stylesheets: false,
+                       javascripts: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
